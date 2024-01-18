e342d2feef88a434597697cd1e6eb61c23d9acc5
JihadOujadi <jihad_oujadi@yahoo.fr>
2024-01-18 09:27:17 +0100

    ajout de la fonction d'apparition

--- script.js
+++ script.js
@@ -1,2 +1,32 @@
+function displayButton() {
+  // Initialize variable
+  const numCheckboxes = document.querySelectorAll(".checkbox-checked").length;
+  let checkboxesChecked = 0;
 
+  // Check if all checkboxes are checked
+  const checkboxes = document.querySelectorAll(".checkbox-checked");
+  for (let i = 0; i < checkboxes.length; i++) {
+    if (checkboxes[i].checked) {
+      checkboxesChecked++;
+    }
+  }
 
+  if (checkboxesChecked === numCheckboxes) {
+    // Display button
+    document.getElementById("bouton").classList.add("button-active");
+    document.getElementById("bouton").classList.remove("bouton");
+  } else {
+    // Hide button
+    document.getElementById("bouton").classList.remove("button-active");
+    document.getElementById("bouton").classList.add("bouton");
+  }
+}
+
+// Add an "OnChange" event to each checkbox
+const checkboxes = document.querySelectorAll("input[type=checkbox]");
+for (let i = 0; i < checkboxes.length; i++) {
+  checkboxes[i].addEventListener("change", displayButton);
+}
+
+// Call function initially
+displayButton();
